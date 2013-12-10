35e2d65fa84d206005beee42e49b07ed13abf26d
zack <zack@pocketchange.com>
2013-12-10 11:33:39 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZackApp::Application.config.secret_key_base = '3a3830b8cf928e8a53ff9523715b7fcf72d9be53ac5dc794f2b43a80ae7700cc373008bb624a146f8e747e1e22dc2c88d06bba5b900abe27a51233ed382a9ebc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZackApp::Application.config.session_store :cookie_store, key: '_zack_app_session'
